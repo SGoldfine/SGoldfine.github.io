883081ed98fe3750a8044099829d9f3232c52467
Spencer Goldfine <goldfis@email.sc.edu>
2023-09-16 18:58:00 -0400

    final commit of assignment 6

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -10,8 +10,25 @@ const moveBall = () => {
     document.getElementById("ball").classList.add("move-ball");
 }
 
+const printComment = () => {
+    document.getElementById("boxes").classList.add("comment");
+
+    const product = document.getElementById("txt-product").value;
+    const header = document.getElementById("add-name");
+    
+    const comment = document.getElementById("txt-comment").value;
+    const rate = document.getElementById("txt-rating").value;
+    const commentP = document.getElementById("add-comment");
+
+    const user = document.getElementById("txt-name").value;
+    const userP = document.getElementById("add-user");
+
+    header.innerHTML += `<section class = "separate"><strong>${product}</strong> <p class = "small">${rate}/5 stars</p> <p class="small">${comment}</p> <p class = "small">by ${user}</p></section>`;
+}
+
 window.onload = () => {
     document.getElementById("button-show").onclick = showPic;
     document.getElementById("button-hide").onclick = hidePic;
     document.getElementById("button-dance").onclick = moveBall;
+    document.getElementById("button-add").onclick = printComment;
 }
