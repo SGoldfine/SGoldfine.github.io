235baaa6c43338ce7bf8c872beda92a1c5d86769
Spencer Goldfine <goldfis@email.sc.edu>
2023-09-30 12:05:03 -0400

    assignment 8 middle commit

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -1,33 +1,53 @@
 const displayDonate = () => {
     const donateAmt = parseInt(document.getElementById("txt-donate").value);
+    const endNumber = 10000;
     const root = document.querySelector(":root");
 
     const error = document.getElementById("error");
     error.classList.add("hidden");
 
-    if(donateAmt < 0) {
+    if(donateAmt < 0 || isNaN(donateAmt) || isNaN(endNumber) || donateAmt > endNumber) {
         error.classList.remove("hidden");
         error.innerHTML = "Not a valid number";
     }
 
-    if(donateAmt == 0) {
-        root.styl.setProperty("--num", "0%");
-    } else if(donateAmt <= 4999 && donateAmt > 0) {
-        root.style.setProperty("--num", "25%");
-    } else if(donateAmt <= 7499 && donateAmt > 0) {
-        root.style.setProperty("--num", "50%");
-    } else if(donateAmt <= 9999 && donateAmt > 0) {
-        root.style.setProperty("--num", "75%");
-    } else if(donateAmt <= 10000 && donateAmt > 0) {
-        root.style.setProperty("--num", "100%");
+    var gradient = 1;
+
+    const setFill = () => {
+        root.style.setProperty("--num", gradient + "px");
     }
+    let bar = document.getElementById("gradient");
+
+    for(let i = 0; i < donateAmt; i++) {
+        const updateFill = setInterval(() => {
+            bar.innerHTML = setFill();
+
+            if (gradient > 10000) {
+                clearInterval(updateFill);
+            }
+        }, 10)  
+    } 
 };
 
-const makeRun = () => {
-    console.log("running");
-};
+var padding = -500;
+
+const moveMan = () => {
+    const setLocation = () => {
+        document.getElementById("img").style.setProperty("--x", padding + "px");
+    }
+
+    let man = document.getElementById("img");
+    const updateMove = setInterval(() => {
+        padding += 10;
+        man.innerHTML = setLocation();
+
+        if (padding >= 400) {
+            clearInterval(updateMove);
+        }
+    }, 50);
+}
 
 window.onload = () => {
     document.getElementById("donate-button").onclick = displayDonate;
-    document.getElementById("img").onclick = makeRun;
+    document.getElementById("img").onclick = moveMan;
 };
