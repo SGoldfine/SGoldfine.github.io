77f5899ac8c2514a0994fd1c5d7cf2d67c554810
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-01 18:07:50 -0400

    assignment 8 final commit

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -1,32 +1,25 @@
 const displayDonate = () => {
     const donateAmt = parseInt(document.getElementById("txt-donate").value);
-    const endNumber = 10000;
     const root = document.querySelector(":root");
 
     const error = document.getElementById("error");
     error.classList.add("hidden");
 
-    if(donateAmt < 0 || isNaN(donateAmt) || isNaN(endNumber) || donateAmt > endNumber) {
+    if(donateAmt < 0 || isNaN(donateAmt) || donateAmt > 10000) {
         error.classList.remove("hidden");
         error.innerHTML = "Not a valid number";
     }
 
-    var gradient = 1;
+    let gradient = 0;
 
-    const setFill = () => {
-        root.style.setProperty("--num", gradient + "px");
-    }
-    let bar = document.getElementById("gradient");
-
-    for(let i = 0; i < donateAmt; i++) {
-        const updateFill = setInterval(() => {
-            bar.innerHTML = setFill();
-
-            if (gradient > 10000) {
-                clearInterval(updateFill);
-            }
-        }, 10)  
-    } 
+    const fill = setInterval(() => {
+        if(donateAmt / 10000 * 100 <= gradient) {
+            clearInterval(fill);
+        } else {
+            gradient++;
+            root.style.setProperty("--num", gradient + "%");
+        }
+    }, 10);
 };
 
 var padding = -500;
@@ -41,6 +34,9 @@ const moveMan = () => {
         padding += 10;
         man.innerHTML = setLocation();
 
+        if(padding % 2 == 0) {
+            img.src = "images/sprintingman.png";
+        }
         if (padding >= 400) {
             clearInterval(updateMove);
         }
