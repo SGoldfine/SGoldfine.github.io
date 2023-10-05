f766329a714cb3a18abf337fc1964852a3630403
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-05 11:28:14 -0400

    added input section to project page

--- projects/part1/projectscript.js
+++ projects/part1/projectscript.js
@@ -34,12 +34,25 @@ const toggleCont = () => {
     document.getElementById("p4of5").classList.add("hidden");
 }
 
+// const printProject = () => {
+//     document.getElementById("boxes").classList.add("comment");
+
+//     const link = document.getElementById("txt-link").value;
+//     const header = document.getElementById("add-link");
+    
+//     const description = document.getElementById("txt-description").value;
+//     const descriptionP = document.getElementById("add-comment");
+
+//     header.innerHTML += `<section class = "separate"><strong>${link}</strong> <p class="small">${description}</p></section>`;
+// }
+
+
 window.onload = () => {
     document.getElementById("nav-toggle").onclick = toggleNav;
     document.getElementById("p1of5").onclick = toggleBio;
     document.getElementById("p2of5").onclick = toggleExp;
     document.getElementById("p3of5").onclick = toggleEdu;
     document.getElementById("p5of5").onclick = toggleCont;
-
+    document.getElementById("button-add").onclick = printProject;
 
 }
