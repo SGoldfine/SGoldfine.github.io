eae46389ecd1b36b93d6d35241e7dc1d032ddee5
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-07 21:46:58 -0400

    assignment 9 final commit

--- assignments/assignment9/script.js
+++ assignments/assignment9/script.js
@@ -13,21 +13,31 @@ const slide = () => {
 }
 
 const displayRainbow = () => {
-    const currentColor = document.querySelector("#colors :first-child");
-    currentColor.classList.remove("hidden");
+    // const button = document.getElementById("rainbow-button");
+    const rainbow = document.querySelector("#rainbow");
+    const pot = document.getElementById("pot");
 
-    let nextColor = currentColor.nextElementSibling;
+    let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+    let position = 0;
 
-    let img = document.getElementById("gold")
-    if(nextColor == null) {
-        img.classList.remove("hidden");
-    }
+    const updateRainbow = setInterval(() => {
+        
+        if(position == colors.length){
+            pot.classList.remove("hidden");
+            clearInterval(updateRainbow);
+        }
 
-    nextColor.classList.remove("hidden");
+        const newColor = document.createElement("p")
+        newColor.classList.add("rainbowColors");
+        newColor.style.setProperty("background", colors[position]);
+
+        position++;
+        rainbow.append(newColor);
+
+    }, 100)
 }
 
 window.onload = () => {
     setInterval(slide, 2000);
     document.getElementById("rainbow-button").onclick = displayRainbow;
-    setInterval(displayRainbow, 2000);
 }
