c5c8a34837a1d66f3692f1c1cdb86e87f369529f
SpencerG3 <spencergold7704@gmail.com>
2023-10-16 20:00:49 -0400

    began work on creating new project

--- projects/part3/project/script.js
+++ projects/part3/project/script.js
@@ -34,17 +34,17 @@ const toggleCont = () => {
     document.getElementById("p4of5").classList.add("hidden");
 }
 
-// const printProject = () => {
-//     document.getElementById("boxes").classList.add("comment");
+const printProject = () => {
+    document.getElementById("boxes").classList.add("comment");
 
-//     const link = document.getElementById("txt-link").value;
-//     const header = document.getElementById("add-link");
+    const newproject = document.getElementById("input");
+
+    const link = document.getElementById("txt-link").value;
     
-//     const description = document.getElementById("txt-description").value;
-//     const descriptionP = document.getElementById("add-comment");
+    const description = document.getElementById("txt-description").value;
 
-//     header.innerHTML += `<section class = "separate"><strong>${link}</strong> <p class="small">${description}</p></section>`;
-// }
+    newproject.innerHTML += `<section class = "separate"><p><strong>${link}</strong></p> <ul class="small"><li>${description}</li></ul></section>`;
+}
 
 
 window.onload = () => {
