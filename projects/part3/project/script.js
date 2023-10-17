4741828fc7fd8bbd54d2cb8ec77b4c50b830cffe
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-17 18:31:23 -0400

    finished iFrame sections

--- projects/part3/project/script.js
+++ projects/part3/project/script.js
@@ -2,36 +2,14 @@ const toggleNav = () => {
     document.getElementById("main-nav-items").classList.toggle("hidden");
 }
 
-const toggleBio = () => {
-    document.getElementById("p1of5").classList.toggle("hidden");
-    document.getElementById("p2of5").classList.add("hidden");
-    document.getElementById("p3of5").classList.add("hidden");
-    document.getElementById("p4of5").classList.add("hidden");
-    document.getElementById("p5of5").classList.add("hidden");
+const showWebFrame = () => {
+    document.getElementById("framea").classList.toggle("hidden");
+    document.getElementById("webpic").classList.toggle("hidden");
 }
 
-const toggleExp = () => {
-    document.getElementById("p2of5").classList.toggle("hidden");
-    document.getElementById("p1of5").classList.add("hidden");
-    document.getElementById("p3of5").classList.add("hidden");
-    document.getElementById("p4of5").classList.add("hidden");
-    document.getElementById("p5of5").classList.add("hidden");
-}
-
-const toggleEdu = () => {
-    document.getElementById("p3of5").classList.toggle("hidden");
-    document.getElementById("p1of5").classList.add("hidden");
-    document.getElementById("p2of5").classList.add("hidden");
-    document.getElementById("p4of5").classList.add("hidden");
-    document.getElementById("p5of5").classList.add("hidden");
-}
-
-const toggleCont = () => {
-    document.getElementById("p5of5").classList.toggle("hidden");
-    document.getElementById("p1of5").classList.add("hidden");
-    document.getElementById("p2of5").classList.add("hidden");
-    document.getElementById("p3of5").classList.add("hidden");
-    document.getElementById("p4of5").classList.add("hidden");
+const showModernFrame = () => {
+    document.getElementById("frameb").classList.toggle("hidden");
+    document.getElementById("modernpic").classList.toggle("hidden");
 }
 
 const printProject = () => {
@@ -48,11 +26,10 @@ const printProject = () => {
 
 
 window.onload = () => {
+    document.getElementById("webpic").classList.remove("hidden");
+    document.getElementById("modernpic").classList.remove("hidden");
     document.getElementById("nav-toggle").onclick = toggleNav;
-    document.getElementById("p1of5").onclick = toggleBio;
-    document.getElementById("p2of5").onclick = toggleExp;
-    document.getElementById("p3of5").onclick = toggleEdu;
-    document.getElementById("p5of5").onclick = toggleCont;
     document.getElementById("button-add").onclick = printProject;
-
+    document.getElementById("weblink").onclick = showWebFrame;
+    document.getElementById("modernlink").onclick = showModernFrame;
 }
