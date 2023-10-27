6b52a7bc884f798b46b57385d522fe24bb8f4ad8
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-27 19:32:31 -0400

    working on loading json

--- projects/part4/class/script.js
+++ projects/part4/class/script.js
@@ -1,7 +1,36 @@
+const getClasses = async () => {
+    const url = "https://sgoldfine.github.io/jsons/classes.json";
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showClasses = async () => {
+    let classes = await getClasses();
+    let classSection = document.getElementById("classes");
+    classes.forEach((class) => 
+      classSection.append(getClass(class))
+    );
+};
+
+const getClass = (class) => {
+    let main = document.getElementById("classes");
+    let section = document.createElement("section");
+    section.classList.add("class");
+
+    h1 = document.createElement("h1");
+    h1.innerText = class.title;
+    main.append(h1);
+
+    return section;
+};
+
 const toggleNav = () => {
     document.getElementById("main-nav-items").classList.toggle("hidden");
 }
-
 
 
 window.onload = () => {
