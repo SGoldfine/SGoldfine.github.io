952309b683fc0e5c39f4b6c523c0c2f7c2657e43
SpencerG3 <spencergold7704@gmail.com>
2023-10-28 16:33:23 -0400

    reset part 4

--- projects/part4/class/script.js
+++ projects/part4/class/script.js
@@ -1,46 +1,7 @@
-const getCourses = async () => {
-    const url = "https://sgoldfine.github.io/jsons/classes.json";
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-const showCourses = async () => {
-    let courses = await getCourses();
-    let courseSection = document.getElementById("courses");
-    courses.forEach((course) => 
-      courseSection.append(getCourse(course))
-    );
-};
-
-const getCourse = (course) => {
-    let section = document.createElement("section");
-    section.classList.add("course");
-
-    let h1 = document.createElement("h1");
-    h1.innerText = course.title;
-    section.append(h1);
-
-    let details = document.createElement("courseDiv");
-    details.classList.add("course-details");
-    section.append(details);
-
-    let p1 = document.createElement("p");
-    p1.innerText = course.course1;
-    details.append(p1);
-
-    return section;
-};
-
 const toggleNav = () => {
     document.getElementById("main-nav-items").classList.toggle("hidden");
 }
 
-
 window.onload = () => {
     document.getElementById("nav-toggle").onclick = toggleNav;
-    showCourses();
 }
