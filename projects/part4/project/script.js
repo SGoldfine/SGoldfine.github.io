8ff1e94fe7efd12b0ec1b9d90c62e78a1d7dbfb9
SpencerG3 <spencergold7704@gmail.com>
2023-10-29 19:50:50 -0400

    fixed add project not working

--- projects/part4/project/script.js
+++ projects/part4/project/script.js
@@ -15,7 +15,7 @@ const toggleNav = () => {
 const printProject = () => {
     document.getElementById("boxes").classList.add("comment");
 
-    const newproject = document.getElementById("new");
+    const newproject = document.getElementById("newproj");
 
     const name = document.getElementById("txt-name").value;
 
