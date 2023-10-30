8fbcef5b28042da2c511b4ca7fdb5f1ed3e19ad0
SpencerG3 <spencergold7704@gmail.com>
2023-10-29 20:05:02 -0400

    fixed leaving page when contact form is submitted

--- projects/part4/contact/script.js
+++ projects/part4/contact/script.js
@@ -2,20 +2,43 @@ const toggleNav = () => {
     document.getElementById("main-nav-items").classList.toggle("hidden");
 }
 
-const logSubmit = () => {
-    const name = document.getElementById("namebox").value;
-    const email = document.getElementById("emailbox").value;
-    const subject = document.getElementById("subjectbox").value;
-    const message = document.getElementById("messagebox").value;
+const showEmailResult = async (e) => {
+    e.preventDefault();
+    const result = document.getElementById("result");
+    let response = await getEmailResult();
+    if (response.status == 200) {
+      result.innerHTML = "Email Successfully Sent";
+    } else {
+      result.innerHTML = "Sorry, your email was not sent.";
+    }
+  };
 
-    console.log(name); 
-    console.log(email); 
-    console.log(subject); 
-    console.log(message); 
-}
+const getEmailResult = async (e) => {
+    const form = document.getElementById("contact-form");
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Please wait...";
+  
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      document.getElementById("result").innerHTML =
+        "Sorry your email couldn't be sent";
+    }
+  };
 
 window.onload = () => {
     document.getElementById("nav-toggle").onclick = toggleNav;
-
-    document.getElementById("submit").onclick = logSubmit;
+    document.getElementById("contact-form").onsubmit = showEmailResult;
 }
